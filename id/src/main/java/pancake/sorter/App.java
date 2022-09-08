16cbc71ea671959823cb0c92eb172c022620828b
Corniels de Best <cornielsdebest@hotmail.com>
2022-09-08 11:48:36 +0200

    init

---
+++ id/src/main/java/pancake/sorter/App.java
@@ -0,0 +1,10 @@
+package pancake.sorter;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
