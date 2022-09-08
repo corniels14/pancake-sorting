79b257c11d543b7d2a25d0642b5f4118caf28a0d
Corniels de Best <cornielsdebest@hotmail.com>
2022-09-08 12:29:33 +0200

    test: added tests

--- id/src/test/java/pancake/sorter/AppTest.java
+++ id/src/test/java/pancake/sorter/AppTest.java
@@ -4,10 +4,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+
 class AppTest {
 
+    @BeforeAll
+    public void init() {
+        //
+    }
+
     @Test
-    void testApp() {
+    void cantAddPancakeToFullPlate() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void pancakesAreDifferentSizes() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void pancakeSorter() {
         assertEquals(1, 1);
     }
 }
