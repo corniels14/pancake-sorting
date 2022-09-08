66e564e9891a99e23da101bc56010c18ed8893be
Corniels de Best <cornielsdebest@hotmail.com>
2022-09-08 13:48:48 +0200

    feat: plate checks and helperfunctions

--- id/src/main/java/pancake/sorter/Plate.java
+++ id/src/main/java/pancake/sorter/Plate.java
@@ -11,17 +11,32 @@ public class Plate {
     }
 
     public Plate(List<Pancake> pancakes){
-        // todo check
-        this.pancakes = pancakes;
+        if (pancakes.size() <= maximumPancakes){
+            this.pancakes = pancakes;
+        }
     }
 
     public boolean addPancake(Pancake pancake){
-        // todo check
-        return this.pancakes.add(pancake);
+        if (pancakeWithDiameterNew(pancake.diameter) && roomForMorePancake()){
+            return this.pancakes.add(pancake);
+        }
+        return false;
     }
 
     public void sort(){
         // todo sort
+    }
+
+    private boolean pancakeWithDiameterNew(int diameter){
+        for (int i = 0; i < this.pancakes.size(); i++){
+            int d = this.pancakes.get(i).diameter;
+            if (d == diameter) return false;
+        }
+        return true;
+    }
+
+    private boolean roomForMorePancake(){
+        return this.pancakes.size() < this.maximumPancakes;
     }
 
 }
